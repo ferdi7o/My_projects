39a633865fd7a30131c86ad68d99031b579249ae
Ferdi Mehmed Akif <76132036+ferdi7o@users.noreply.github.com>
2025-03-26 02:56:26 +0200

    get from user name and print this name with wrong message

--- project_window.py
+++ project_window.py
@@ -5,10 +5,17 @@ window = tk.Tk()
 window.title("Ferdi's Window Page")
 window.geometry("400x300")
 
-def botton_get():
-    messagebox.showinfo("information", "You reach botton!")
+etiket = tk.Label(window, text="Your name:")
+etiket.pack(pady=5)
 
-botton = tk.Button(window, text="Click this Botton!", command=botton_get())
-botton.pack(pady=20)
+entry_box = tk.Entry(window)
+entry_box.pack(pady=5)
+
+def show_message():
+    mesage = entry_box.get()
+    messagebox.showinfo("WRONG - information", f"Your name is {mesage}")
+
+buton = tk.Button(window, text="Show message!", command=show_message)
+buton.pack(pady=10)
 
 window.mainloop()
