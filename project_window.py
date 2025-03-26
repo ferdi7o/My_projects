8aabac43ec34b09c1d296e9734df47a694a362a0
Ferdi Mehmed Akif <76132036+ferdi7o@users.noreply.github.com>
2025-03-26 02:31:33 +0200

    create a button
    crated a button with text

--- project_window.py
+++ project_window.py
@@ -2,7 +2,13 @@ import tkinter as tk
 from tkinter import messagebox
 
 window = tk.Tk()
-window.title("Ferdi's Window")
+window.title("Ferdi's Window Page")
 window.geometry("400x300")
+
+def botton_get():
+    messagebox.showinfo("information", "You reach botton!")
+
+botton = tk.Button(window, text="Click this Botton!", command=botton_get())
+botton.pack(pady=20)
 
 window.mainloop()
